f18a5ff488179d9b0350c4ec6eb5848c2ff12429
omar <oshawky103@gmail.com>
2025-07-27 22:38:27 +0300

    modified

--- spinWords.js
+++ spinWords.js
@@ -1,19 +1,19 @@
 // Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
 
-
-function spinWords(string) {
-         // coding here
-}
 // steps
 //  1- splite on word
 // 2 - filteration on array condition(length >= 5) return word
 // 3 - reverse word and join them
 
+function spinWords(string) {
+  // coding here
+  const spin = string
+    .split(" ")
+    .map((w) => {
+      return w.length >= 5 ? w.split("").reverse().join("") : w;
+    })
+    .join(" ");
+  return spin;
+}
+
 console.log(spinWords("Hell is Married"));
-const spin = string
-  .split(" ")
-  .map((w) => {
-    return w.length >= 5 ? w.split("").reverse().join("") : w;
-  })
-  .join(" ");
-return spin;
